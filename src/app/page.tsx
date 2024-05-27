436a7cd7c2f155e410c3923ba0f0466a45d6290b
Monika Zyznowska <monika.zyznowska@elpassion.pl>
2024-05-27 16:28:34 +0200

    Change logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import MyButton from "@/helpers/button";
+import MyButton from "@/components/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       
       <MyButton />
     </main>
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ImageLike, createWorker } from "tesseract.js";
 
 const MyButton = () => {
   const [selectedFile, setSelectedFile] = useState<ImageLike>("");
+  const [text, setText] = useState<string>("");
 
   const handleFileChange = (event: any) => {
     const file = event.target.files[0];
@@ -12,25 +13,33 @@ const MyButton = () => {
 
     setSelectedFile(file);
   };
+
   const handleClick = async () => {
     const worker = await createWorker("eng");
     const ret = await worker.recognize(selectedFile);
     console.log(ret.data.text);
+    setText(ret.data.text);
     await worker.terminate();
   };
+
+  useEffect(() => {
+    handleClick();
+  }, [selectedFile]);
   return (
     <>
-      <label className="cameraButton">
-        Take a picture
+      <label className="bg-white text-black py-4 px-8 mb-4 cameraButton">
+        Read text from photo
         <input
           type="file"
           accept="image/*;capture=camera"
           onChange={handleFileChange}
+          className="hidden"
         />
       </label>
-      <button className="bg-white p-5 text-black" onClick={handleClick}>
-        Click me
-      </button>
+      <p className="p-4 border">{text}</p>
+      {/* <button className="bg-white p-5 mt-10 text-black" onClick={handleClick}>
+        Recognize text
+      </button> */}
     </>
   );
 };
