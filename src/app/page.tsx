4306dca2131a5791cc3f167e0a55dde5ab359d8a
Monika Zyznowska <monika.zyznowska@elpassion.pl>
2024-06-13 13:27:01 +0200

    Add styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,9 @@ import MyButton from "@/components/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      
-      <MyButton />
-      <div className="flex flex-col justify-between">
+    <main className="flex  flex-col items-center p-24 gap-3">
       <div className="border-solid border-2 w-5 h-[100dvh]"></div>
       <div className="border-solid border-2 w-5 h-full"></div>
-      </div>
     </main>
   );
 }
