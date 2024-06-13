3f091dc3420378f759c9df3f92eb80145ba38522
Monika Zyznowska <monika.zyznowska@elpassion.pl>
2024-06-13 13:49:35 +0200

    Change style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import MyButton from "@/components/button";
 
 export default function Home() {
   return (
-    <main className="flex justify-evenly items-center gap-3">
-      <div className="border-solid border-2 w-5 h-[100dvh] bg-red-500"></div>
+    <main className="flex justify-evenly bg-white items-center gap-3">
+      <div className="border-solid border-2 border-black w-5 h-[100dvh] bg-red-500"></div>
       <div className="border-solid border-2 w-5 h-[100vh] bg-green-500"></div>
     </main>
   );
