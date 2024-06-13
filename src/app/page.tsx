40c0d5d20ecbc85880b09c5ddf26e51719d71365
Monika Zyznowska <monika.zyznowska@elpassion.pl>
2024-06-13 13:27:57 +0200

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import MyButton from "@/components/button";
 
 export default function Home() {
   return (
-    <main className="flex  flex-col items-center p-24 gap-3">
+    <main className="flex justify-evenly items-center p-24 gap-3">
       <div className="border-solid border-2 w-5 h-[100dvh]"></div>
       <div className="border-solid border-2 w-5 h-full"></div>
     </main>
